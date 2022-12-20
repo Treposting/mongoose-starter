cde814aa5f5ba0a4441092b08d862269ed3cac54
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-12-20 14:10:46 +0600

    added => vercel config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import cors from "cors";
 import "dotenv/config";
 import express, { Application, Request, Response } from "express";
+import path from "path";
 import dbConnect from "./utils/dbConnect";
+import log from "./utils/logger";
 const app: Application = express();
-
 /* middleware  */
 app.use(cors());
 app.use(express.json());
@@ -15,12 +16,13 @@ try {
   log.error(`Database error: ${error}`);
 }
 // set the view engine to ejs
+app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 /* here will be all the imports routes */
 import productRoute from "./routes/v1/productRoute";
 import testRoute from "./routes/v1/test";
-import log from "./utils/logger";
 
 /* here will be the all the routes */
 app.get("/", (req: Request, res: Response) => {
